6ce21e9903defe3fd88667337aecc5c6fda3e258
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-05-29 11:40:03 +0300

    6 Числа от 1 до 49
    https://youtu.be/HWu24SJH_9s?t=1h32m38s

---
+++ TotoRandomNumbers_6_49/TotoRandomNumbers_6_49/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotoRandomNumbers_6_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+
+            for (int i = 1; i < 7; i++)
+            {
+                int randomNumber = generator.Next(1, 50);
+                Console.WriteLine($"Number {randomNumber}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
